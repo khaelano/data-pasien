6bca20c14cad01274a1db8347ddefff47f3d96bb
Khaelano Abroor Maulana <khaelanoabroor@gmail.com>
2023-11-07 08:14:34 +0700

    initial commit

---
+++ src/App.java
@@ -0,0 +1,14 @@
+// Membuat aplikasi sederhana untuk membantu dokter mendiri mencatat data pasien
+// dan memproses hasil pemeriksaan. Aplikasi in harus mampu melakukan:
+// 1. Menyimpan data paien (nama. usia, jenis kelamin)
+// 2. Memasukkan hasil pemeriksaan (tekanan darah, suhu tubuh, dan tinggi badan)
+// 3. Menghitung rata-tata tekanan darah, suhu tubuh, dan tinggi badan dari semua pasien
+// Pastijan mengunakan variabel, konstanta, perulangan, percabangan, array serta methods
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner S = new Scanner(System.in);
+    }
+}
