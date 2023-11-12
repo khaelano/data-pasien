00bd9cb279bf27ccd4f7c0ded0760a5e6d22c1f3
Khaelano Abroor Maulana <khaelanoabroor@gmail.com>
2023-11-12 22:54:32 +0700

    penambahan method appendData

--- src/App.java
+++ src/App.java
@@ -7,13 +7,48 @@
 
 import java.util.Scanner;
 
+import javax.management.Query;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-    Scanner input = new Scanner (System.in);
-        while (true){
-            System.out.printf("%-13s:", "Nama ");
-            String name = input.nextLine();
-            
-        }    
+  public static void main(String[] args) throws Exception {
+    Scanner input = new Scanner(System.in);
+    while (true) {
+      System.out.println("Menu:");
+      System.out.println("1. Input Data Pasien");
+      System.out.println("2. Input Hasil Pemeriksaan");
+      System.out.println("3. Hitung Rata-rata Hasil Pemeriksaan Seluruh Pasien");
+
+      System.out.print("Masukkan Pilihan anda: ");
+      int pilihan = input.nextInt();
+
+      String data[] = new String[0];
+
+      switch (pilihan) {
+        case 1:
+          System.out.println("Masukkan Nama Pasien: ");
+          break;
+
+        default:
+          break;
+      }
+
+      System.out.printf("%-13s:", "Nama ");
+      String name = input.nextLine();
+
     }
+  }
+
+  public static void inputData() {
+    System.out.print("Masukkan Data Pasien: ");
+
+  }
+
+  public static String[] appendData(String[] oldArray, String newData) {
+    String[] newArray = new String[oldArray.length + 1];
+    for (int i = 0; i < oldArray.length; i++) {
+      newArray[i] = oldArray[i];
+    }
+    newArray[oldArray.length] = newData;
+    return newArray;
+  }
 }
