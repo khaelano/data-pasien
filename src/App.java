fcb1d1202ce942a120fdb79cc560ff5737b43e0b
Khaelano Abroor Maulana <khaelanoabroor@gmail.com>
2023-11-20 20:21:51 +0700

    wkwkwkwk

--- src/App.java
+++ src/App.java
@@ -7,46 +7,98 @@
 
 import java.util.Scanner;
 
-import javax.management.Query;
-
 public class App {
   public static void main(String[] args) throws Exception {
     Scanner input = new Scanner(System.in);
     while (true) {
       System.out.println("Menu:");
       System.out.println("1. Input Data Pasien");
-      System.out.println("2. Input Hasil Pemeriksaan");
-      System.out.println("3. Hitung Rata-rata Hasil Pemeriksaan Seluruh Pasien");
+      System.out.println("2. Hitung Rata-rata Hasil Pemeriksaan Seluruh Pasien");
 
       System.out.print("Masukkan Pilihan anda: ");
       int pilihan = input.nextInt();
 
-      String data[] = new String[0];
+      String dataPasien[][] = new String[1][1];
+      int hasilPemeriksaan[][] = new int[1][1];
 
       switch (pilihan) {
         case 1:
-          System.out.println("Masukkan Nama Pasien: ");
+          // mengambil data pasien 
+          String[] data = inputData(input);
+          dataPasien = appendDataString(dataPasien, data);
+        
+          break;
+
+        case 2:
+          // mengambil hasil pemeriksaan pasien
+          int[] hasil = inputHasilPemeriksaan(input);
+          hasilPemeriksaan = appendDataInt(hasilPemeriksaan, hasil);
+          break;
+
+        case 3:
+
           break;
 
         default:
           break;
       }
-
       System.out.printf("%-13s:", "Nama ");
       String name = input.nextLine();
-
     }
   }
 
-  public static void inputData() {
-    System.out.print("Masukkan Data Pasien: ");
+  public static String[] inputData(Scanner input) {
+    String[] dataPasien = new String[3];
 
+    System.out.print("Masukkan nama pasien: ");
+    String nama = input.nextLine();
+    dataPasien[0] = nama;
+
+    System.out.println("Masukkan usia pasien: ");
+    String usia = input.nextLine();
+    dataPasien[1] = usia;
+
+    System.out.println("Masukkan jenis kelamin pasien: ");
+    String gender = input.nextLine();
+    dataPasien[2] = gender;
+
+    return dataPasien;
   }
 
-  public static String[] appendData(String[] oldArray, String newData) {
-    String[] newArray = new String[oldArray.length + 1];
+  public static int[] inputHasilPemeriksaan(Scanner input) {
+    int[] hasilPemeriksaan = new int[3];
+
+    System.out.println("Masukkan tekanan darah pasien: ");
+    hasilPemeriksaan[0] = input.nextInt();
+
+    System.out.println("Masukkan suhu tubuh pasien: ");
+    hasilPemeriksaan[1] = input.nextInt();
+
+    System.out.println("Masukkan tinggi badan pasien: ");
+    hasilPemeriksaan[2] = input.nextInt();
+
+    return hasilPemeriksaan;
+  }
+
+  public static String[][] appendDataString(String[][] oldArray, String[] newData) {
+    /**
+     * Method untuk menambah elemen (array String) pada sebuah array String
+     */
+    String[][] newArray = new String[oldArray.length + 1][2];
     for (int i = 0; i < oldArray.length; i++) {
-      newArray[i] = oldArray[i];
+      newArray[i][0] = oldArray[i][0];
+    }
+    newArray[oldArray.length] = newData;
+    return newArray;
+  }
+
+  public static int[][] appendDataInt(int[][] oldArray, int[] newData) {
+    /**
+     * Method untuk menambah elemen (array String) pada sebuah array String
+     */
+    int[][] newArray = new int[oldArray.length + 1][2];
+    for (int i = 0; i < oldArray.length; i++) {
+      newArray[i][0] = oldArray[i][0];
     }
     newArray[oldArray.length] = newData;
     return newArray;
